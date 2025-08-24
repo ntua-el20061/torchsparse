a2d1a49ce9bcb4234058f5a2623cbb02dbc0c494
ntua-el20061 <el20061@mail.ntua.gr>
2025-08-24 18:36:54 +0300

    modified:   torchsparse/backend/others/sparsemapping_cuda.cu

--- torchsparse/backend/others/sparsemapping_cuda.cu
+++ torchsparse/backend/others/sparsemapping_cuda.cu
@@ -475,6 +475,7 @@ std::vector<at::Tensor> build_kernel_map_downsample_hashmap(
   }
   // stage2: get unique coordinates and insert them to the grid.
   int n_out_points_with_duplicate = _n_out_points.item<int>();
+  std::printf("KEKW: %d", n_out_points_with_duplicate);
   at::Tensor _out_coords = std::get<0>(torch::_unique(torch::from_blob(transformed_out_coords, {n_out_points_with_duplicate}, options_long)));
   int64_t *out_coords = _out_coords.data_ptr<int64_t>();
 
